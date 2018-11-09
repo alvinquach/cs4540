fc0e9fc3adf99dd509a9a097aa0be2812f462294
Unknown <alvinquach91@gmail.com>
2018-11-08 23:58:06 -0800

    hw1: added NewsItem model and JSON parser

--- homework1/app/src/main/java/com/alvinquach/newsapp/util/JsonUtils.java
+++ homework1/app/src/main/java/com/alvinquach/newsapp/util/JsonUtils.java
@@ -1,13 +1,33 @@
 package com.alvinquach.newsapp.util;
 
-import android.util.Log;
+import com.alvinquach.newsapp.NewsItem;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class JsonUtils {
 
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static ArrayList<NewsItem> parseNews(String jsonString) {
+        ArrayList<NewsItem> result = new ArrayList<>();
+        try {
+            JsonNode jsonNode = mapper.readTree(jsonString);
+            JsonNode articles = jsonNode.get("articles");
+            if (articles != null && articles.isArray()) {
+                for (JsonNode article : articles) {
+                    NewsItem newsItem = mapper.treeToValue(article, NewsItem.class);
+                    result.add(newsItem);
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
 }
 
